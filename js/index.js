d38ddcf875b384f0bddd7bd0a026ef1502a79b6b
Yandira <yandirachoque@gmail.com>
2024-12-17 20:36:23 -0300

    Modificamos el javascript de la pagina principal

--- js/index.js
+++ js/index.js
@@ -1,30 +1,34 @@
-// variable global para almacenar los productos seleccionados
-let carrito = [];
+// Variable global para almacenar los productos seleccionados
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-const agregarAlcarrito = (nombre,precio) =>{
-    //agregar el producto como un objeto al carrito
-    carrito.push({nombre,precio})
+// Función para agregar productos al carrito
+const agregarAlCarrito = (nombre, imagen, precio) => {
+    // Agregar el producto como un objeto al carrito
+    carrito.push({nombre, imagen, precio});
 
-    // actualizar el contador visual del carrito
-    actualizarContador()
-    // muestra un alerta de confirmacion
-    alert(`Agregaste : ${nombre} al carrito`)
-}
+    // Actualizar el contador visual del carrito
+    actualizarContador();
 
+    // Mostrar una alerta de confirmación
+    alert(`Agregaste: ${nombre} al carrito`);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
 
-// funcion para actualizar el contador del carrito
-const actualizarContador = ()=>{
-    //cambiamos el contenido del HTML con el ID contador-carrito
-    document.getElementById("contador-carrito").textContent = carrito.length
+// Función para actualizar el contador del carrito
+const actualizarContador = () => {
+    // Cambiar el contenido del HTML con el ID contador-carrito
+    document.getElementById("contador-carrito").textContent = carrito.length;
+};
 
-}
-
-// Guarda el contenido del carrito en el almacenamiento local antes de cerrar la pagina
-
-window.addEventListener("beforeunload",()=>{
-localStorage.setItem("carrito",JSON.stringify(carrito))
+// Guarda el contenido del carrito en el almacenamiento local antes de cerrar la página
+window.addEventListener("beforeunload", () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 });
 
+// Inicializar el contador del carrito al cargar la página
+document.addEventListener("DOMContentLoaded", () => {
+    actualizarContador();
+});
 
 
 
