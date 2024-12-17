49e19d96294f0cdced02a42b2f0784a59a6c79e1
Yandira <yandirachoque@gmail.com>
2024-12-17 20:33:35 -0300

    Agregamos el archivo javascript del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,61 +1,67 @@
 // recuperar el carrito del almacenamiento local
-
 const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+console.log(carrito);
 
 // Muestra los productos en el carrito
-
 const mostrarCarrito = () => {
-    const lista = document.getElementById("lista-carrito")
+    const lista = document.getElementById("lista-carrito");
     lista.innerHTML = " ";
 
     if (carrito.length === 0) {
         lista.innerHTML = '<p>Tu carrito está vacio</p>';
+        actualizarResumen();
         return;
     }
     carrito.forEach((item, indice) => {
-        const producto = document.createElement("article")
-        producto.classList.add("producto")
+        const producto = document.createElement("div");
+        producto.classList.add("producto");
         producto.innerHTML = `
-        <h2>${item.nombre}</h2>
-        <p class="precio">$${item.precio}</p>
+        <div>
+            <h2>${item.nombre}</h2>
+            <img src=${item.imagen} alt="${item.nombre}">
+            <p class="precio">$${item.precio}</p>
+        </div>
         <button onclick="eliminarDelCarrito(${indice})">Eliminar</button>
         `;
-        lista.appendChild(producto)
+        lista.appendChild(producto);
     });
-}
+    actualizarResumen();
+};
+
+// Actualiza el resumen del carrito
+const actualizarResumen = () => {
+    const totalProductos = document.getElementById("total-productos");
+    const importeTotal = document.getElementById("importe-total");
+
+    const total = carrito.reduce((acc, item) => acc + item.precio, 0);
+    totalProductos.textContent = carrito.length;
+    importeTotal.textContent = total.toFixed(2);
+
+    const botonCompra = document.querySelector("button[onclick=`realizarCompra()`]");
+    const resumenCarrito = document.getElementById("resumen-carrito");
+
+    resumenCarrito.appendChild(botonCompra);
+};
 
 //Elimina un producto del carrito
 const eliminarDelCarrito = (indice) => {
     carrito.splice(indice, 1);
-    localStorage.setItem("carrito", JSON.stringify(carrito))
+    localStorage.setItem("carrito", JSON.stringify(carrito));
     mostrarCarrito();
-}
+};
 
 // Simule la compra
-
 const realizarCompra = () => {
-    alert("compra realizada con exito")
-    localStorage.removeItem("carrito")
-    window.location.href = "index.html"
+    alert("Compra realizada con éxito");
+    localStorage.removeItem("carrito");
+    window.location.href = "index.html";
 }
 
 // inicializar el carrito al carga la pagina
-mostrarCarrito()
+document.addEventListener("DOMContentLoaded",() =>{
+    mostrarCarrito();
 
-let handle = null;
+});
 
-async function doClick() {
-    if (!handle) {
-        try {
-            handle = await document.requestStorageAccess({ localStorage: true });
-        } catch (err) {
-            // Access was not granted.
-            return;
-        }
-    }
 
-    // Use handle to access unpartitioned local storage.
-    handle.localStorage.setItem('foo', 'bar');
-}
 
-document.querySelector('#my-button').addEventListener('click', doClick);
